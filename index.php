c4e0d716236007ee1589f32ea63bd104b13fda2e
caringpaws-ph <94284006+caringpaws-ph@users.noreply.github.com>
2021-11-15 02:51:22 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1419,6 +1419,8 @@ table{
     <script src="./assets/js/main.js"></script>
     
     <script src="./js/fbc.js"></script>
+    <script type="text/javascript" src="./js/showeduc.js"></script>
+    
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
     <script>
@@ -1434,7 +1436,7 @@ table{
 
     <!-- Your Chat Plugin code -->
     <div id="fb-customer-chat" class="fb-customerchat"></div>
-    <script type="text/javascript" src="./js/showeduc.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-    </body>
+
+    
+</body>
 </html>
