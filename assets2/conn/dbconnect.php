eda2c56a11f7840035f0801a1390580d4a6ec248
caringpaws-ph <94284006+caringpaws-ph@users.noreply.github.com>
2021-11-14 19:44:20 +0800

    UPDATE

--- assets2/conn/dbconnect.php
+++ assets2/conn/dbconnect.php
@@ -14,9 +14,11 @@ server with default setting (user 'root' with no password) */
 //define('DB_NAME', 'db_healthcare');
 
 //define('DB_SERVER', 'remotemysql.com');
-define('DB_USERNAME', 'UPc1x6hhWL');
-define('DB_PASSWORD', 'xaeSCnp1RQ');
-define('DB_NAME', 'UPc1x6hhWL');
+define('DB_SERVER', 'sql6.freemysqlhosting.net');
+define('DB_USERNAME', 'sql6451028');
+define('DB_PASSWORD', 'UssyZb8LGc');
+define('DB_NAME', 'sql6451028');
+ 
 /* Attempt to connect to MySQL database */
 $con = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
