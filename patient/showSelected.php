a319802d4d554708ffedb7a42076059b5dbcdd02
caringpaws-ph <94284006+caringpaws-ph@users.noreply.github.com>
2021-11-14 21:23:50 +0800

    Update showSelected.php

--- patient/showSelected.php
+++ patient/showSelected.php
@@ -4,7 +4,7 @@
 
     $P = isset($_POST['x']) ? $_POST['x'] : null;
 
-    $con22 = mysqli_connect("localhost", "root", "", "db_healthcare");
+    $con22 = mysqli_connect("sql6.freemysqlhosting.net", "sql6451028", "UssyZb8LGc", "sql6451028");
     $sql22 = "SELECT * from petprofile WHERE Pname = '$P'";
     $res22 = mysqli_query($con22, $sql22);
 
