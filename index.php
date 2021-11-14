89ba66538034f3ce055c2a67a80a7d9e4eab1286
caringpaws-ph <94284006+caringpaws-ph@users.noreply.github.com>
2021-11-14 18:09:04 +0800

    Updated

--- index.php
+++ index.php
@@ -329,7 +329,7 @@ table{
 
 <body>
 
-    <!-- Preloader Start -->
+    <!-- Preloader Start 
     <div id="preloader-active">
         <div class="preloader d-flex align-items-center justify-content-center">
             <div class="preloader-inner position-relative">
@@ -340,7 +340,7 @@ table{
             </div>
         </div>
     </div>
-    <!-- Preloader Start -->
+     Preloader Start -->
 
     <header>
 
